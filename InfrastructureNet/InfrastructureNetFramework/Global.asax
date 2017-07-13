d59162669129627e0001c6514458d09aab8476f6
sergey.gupa <sergey.gupa@gmail.com>
2017-07-13 17:20:51 +0300

    chnaged core to new

---
+++ InfrastructureNet/InfrastructureNetFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InfrastructureNetFramework.MvcApplication" Language="C#" %>
---
+++ InfrastructureNet/InfrastructureNetFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfrastructureNetFramework.Startup))]
+namespace InfrastructureNetFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
